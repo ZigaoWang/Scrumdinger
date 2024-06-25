c0334949d32beecb41b5028e089496e77195324d
ZigaoWang <zigao.wang24@gmail.com>
2024-06-25 22:00:15 +0800

    Update

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,7 +11,16 @@ import SwiftUI
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
     }
 }
 
@@ -20,5 +29,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
     static var previews: some View {
         CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
