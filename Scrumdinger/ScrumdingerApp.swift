cb6da53b6ab3ba8ca96a9413f9f0e8aea2f015bf
ZigaoWang <zigao.wang24@gmail.com>
2024-06-25 19:25:14 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Zigao Wang on 2024/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Zigao Wang on 2024/6/25.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
