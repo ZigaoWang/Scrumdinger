a271dd5eb965afaae9c13e2978922a798bb9ac20
ZigaoWang <zigao.wang24@gmail.com>
2024-06-26 16:52:30 +0800

    Update DefaultView

---
+++ Scrumdinger/DefaultView.swift
@@ -0,0 +1,38 @@
+//
+//  DefaultView.swift
+//  Scrumdinger
+//
+//  Created by Zigao Wang on 2024/6/26.
+//
+
+import SwiftUI
+
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+  
+    var body: some View {
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+        }
+    }
+}
+
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
